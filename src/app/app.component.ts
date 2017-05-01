29ef24161849e8da4a6eda87d2e2446c1542b623
Matheus Marinho <matheuzmarinho@gmail.com>
2017-05-01 13:33:44 -0300

    Projeto Semana da Computação Inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  meuNome="Matheus Marinho"
+  imagem = 'https://i.ytimg.com/vi/FAciZRkOKQs/maxresdefault.jpg'
+  personagem = 'Groot';
+  input :boolean = true;
+  imagemVisivel :boolean = false;
+  nomesAmigos = ['Senhor das Estrelas','Gamora','Rocket','Nebulosa']
+  habilidades = [{id:1,habilidade:'Regeneração'},{id:2,habilidade:'Elasticidade'},{id:3,habilidade:'Absorver Madeira'}]
+
+  getFilme(){
+    return "Guardiões da Galáxia Vol.2"
+  }
+
+  getPoder(poder){
+    console.log(poder)
+  }
+
+  aparecerImagem(){
+    this.imagemVisivel = !this.imagemVisivel
+  }
+
+  mostrarHabilidade(habilidade){
+    console.log(habilidade)
+  }
 }
